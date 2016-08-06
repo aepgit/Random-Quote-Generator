7e49c152b647285522d27fa7cd25b06c5977b8d6
Antti <aepohjola@gmail.com>
2016-08-06 20:02:57 +0300

    Functional version.

---
+++ js/quotes.js
@@ -0,0 +1,59 @@
+var quotes = [ 
+  { 
+    quote: 'Practice does not make perfect. Only perfect practice makes perfect.',
+    source: 'Vince Lombardi',
+    tags: [ "inspirational" ]
+  },
+  { 
+    quote: 'Time is the most valuable thing a man can spend.',
+    source: 'Theophrastus',
+    tags: [ "inspirational" ]
+  },
+  { 
+    quote: "The universe doesn't allow perfection.",
+    source: 'Stephen Hawking',
+    citation: 'A Brief History of Time',
+    year: 1987,
+    tags: [ "inspirational" ]
+  },
+  { 
+    quote: "Maybe ever’body in the whole damn world is scared of each other.",
+    source: 'John Steinbeck',
+    citation: 'Of Mice And Men',
+    year: 1937,
+    tags: [ "funny", "silly" ]
+  },
+  { 
+    quote: 'It sounds plausible enough tonight, but wait until tomorrow. Wait for the common sense of the morning.',
+    source: 'H.G. Wells',
+    citation: 'The Time Machine',
+    year: 1895,
+    tags: [ "funny", "sensible" ]
+  },
+  { 
+    quote: "It's much better to do good in a way that no one knows anything about it.",
+    source: 'Leo Tolstoy',
+    citation: 'Anna Karenina',
+    year: 1877,
+    tags: [ "inspirational" ]
+  },
+  { 
+    quote: "It's much better to do good in a way that no one knows anything about it.",
+    source: 'Leo Tolstoy',
+    citation: 'Anna Karenina',
+    year: 1877,
+    tags: [ "inspirational" ]
+  },
+  { 
+    quote: "We can experience nothing but the present moment, live in no other second of time, and to understand this is as close as we can get to eternal life.",
+    source: 'P.D. James',
+    citation: 'The Children Of Men',
+    year: 1992,
+    tags: [ "inspirational", "existential" ]
+  },
+  { 
+    quote: "What is better - to be born good, or to overcome your evil nature through great effort?",
+    source: 'Paarthurnax',
+    tags: [ "inspirational", "motivational" ]
+  }
+];
--- js/script.js
+++ js/script.js
@@ -2,3 +2,76 @@
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
+function printQuote() {
+	// Reset the automatic reload interval
+	resetInterval();
+	// Set a random background color
+	document.body.style.backgroundColor = getRandomColor();
+
+	// Get a random quote
+	var quote = getRandomQuote();
+	var message = "";
+
+	// Add the valid fields of the quote to the message variable with formatting
+	message += "<p class='quote'> " + quote.quote + " </p>";
+	message += "<p class='source'> " + quote.source;
+	if(quote.citation != null) {
+		message += "<span class='citation'> " + quote.citation + " </span> ";
+	}
+	if(quote.year != null) {
+		message += "<span class='year'> " + quote.year + " </span> ";
+	}
+	message += "</p>";
+
+	// Set the target div and the message in it
+	var outputDiv = document.getElementById('quote-box');
+	outputDiv.innerHTML = message;
+}
+
+// Arrays to make sure each quote is used only once in one iteration
+var quotesUnused = [];
+var quotesUsed = [];
+// Return a random quote from the quotes array. 
+// Only use each quote in the array once, before starting from the beginning.
+function getRandomQuote() {
+
+	// Check if the unused quotes array is empty.
+	// If it is, fill it with all the quotes and clear the quotesUsed array.
+	if(quotesUnused.length == 0)
+	{
+		quotesUnused = quotes.slice();
+		quotesUsed = [];
+	}
+
+	// Find a random quote from the array
+	var i = Math.floor(Math.random() * quotesUnused.length);
+	var quote = quotesUnused[i];
+
+	// Remove the chosen quote from the Unused array and add it to the Used array.
+	quotesUnused.splice(i,1);
+	quotesUsed.push(quote);
+
+	return quote;
+}
+
+// Nice calm backgroundcolors
+var validColors = ["#56ab2f", "#a8e063", "#36b55c", "#457fca", "#5691c8"];
+// Return a random color from the valid color palette described in validColors
+function getRandomColor() {
+	var i = Math.floor(Math.random() * validColors.length);
+	return validColors[i];
+}
+
+// Create an interval at which to call the printQuote function
+var intervalID = window.setInterval(printQuote, 30000);
+// Clears the interval and resets it to 30 seconds.
+function resetInterval() {
+	if(intervalID != null){
+		window.clearInterval(intervalID);
+	}
+
+	intervalID = window.setInterval(printQuote, 30000);
+}
+
+// Print the first quote.
+printQuote();
